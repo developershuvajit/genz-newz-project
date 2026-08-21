6b5770c24b405e7285cfcb37316534274eb75c5d
developershuvajit <karmakarshuvajit199@gmail.com>
2026-08-21 22:25:55 +0530

    core controller created okay

---
+++ core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+class Controller {
+    protected function view($view, $data = []) {
+        extract($data);
+        $viewPath = VIEWS_PATH . '/' . $view . '.php';
+        if (file_exists($viewPath)) {
+            require_once $viewPath;
+        } else {
+            die("View not found: $view");
+        }
+    }
+    
+    protected function json($data) {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
+    
+    protected function redirect($url) {
+        Helper
