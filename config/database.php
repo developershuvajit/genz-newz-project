9adfa50c97b6b1212e0de35f67dde36317f61000
developershuvajit <karmakarshuvajit199@gmail.com>
2026-08-21 22:18:43 +0530

    database connected okay  bro

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'genz');
+define('DB_USER', 'root'); // Change for production
+define('DB_PASS', 'root'); // Change for production
+define('DB_CHARSET', 'utf8mb4');
+?>
