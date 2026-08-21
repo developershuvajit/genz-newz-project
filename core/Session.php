6e7591b1323d26c797e6cdb9ea4153ba8fa7dd26
developershuvajit <karmakarshuvajit199@gmail.com>
2026-08-21 22:21:39 +0530

    session set

---
+++ core/Session.php
@@ -0,0 +1,40 @@
+<?php
+class Session {
+    public static function start() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+    
+    public static function set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+    
+    public static function get($key, $default = null) {
+        return $_SESSION[$key] ?? $default;
+    }
+    
+    public static function has($key) {
+        return isset($_SESSION[$key]);
+    }
+    
+    public static function remove($key) {
+        unset($_SESSION[$key]);
+    }
+    
+    public static function destroy() {
+        session_destroy();
+        $_SESSION = [];
+    }
+    
+    public static function setFlash($key, $value) {
+        $_SESSION['_flash'][$key] = $value;
+    }
+    
+    public static function getFlash($key, $default = null) {
+        $value = $_SESSION['_flash'][$key] ?? $default;
+        unset($_SESSION['_flash'][$key]);
+        return $value;
+    }
+}
+?>
